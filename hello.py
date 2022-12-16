b788ea7a768768c495ed5075c6c0628dde941551
dermie83 <dermiemadsen@gmail.com>
2022-12-16 18:55:33 +0000

    create forms

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "arse_buscuits"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name? ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 @app.route('/')
 
@@ -35,6 +46,11 @@ def page_not_found(e):
 def server_error(e):
     return render_template("500.html"), 500
 
+
+# Create a Name Page
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    return render_template("name.html")
 
 
 if __name__ == '__main__':
