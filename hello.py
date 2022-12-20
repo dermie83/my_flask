85d9b2444d3b6470dbff5c643018019d745af08e
dermie83 <dermiemadsen@gmail.com>
2022-12-20 18:30:44 +0000

    create databse with sqlalchemy

--- hello.py
+++ hello.py
@@ -2,10 +2,44 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
-
+# Add Databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# Secret key!
 app.config['SECRET_KEY'] = "arse_buscuits"
+# initialise the Database
+db = SQLAlchemy(app)
+
+# Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.now)
+
+    # create string to display data
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# with app.app_context():
+#     db.create_all()
+
+#     db.session.add(Users(name="example 1", email = 'dermiemadsen@gmail.com'))
+#     db.session.commit()
+
+#     users = db.session.execute(db.select(Users)).scalars()
+
+
+# Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("User name? ", validators=[DataRequired()])
+    email = StringField("User email? ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a Form Class
 class NamerForm(FlaskForm):
@@ -13,11 +47,12 @@ class NamerForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+@app.route('/user/add', methods = ['GET', 'POST'])
+def add_user():
+    return render_template("add_user.html")
+
+
 @app.route('/')
-
-# def index():
-#     return ("Hellooo World!!!")
-
 def index():
     first_name = "Susan"
     last_name = "Parsons"
